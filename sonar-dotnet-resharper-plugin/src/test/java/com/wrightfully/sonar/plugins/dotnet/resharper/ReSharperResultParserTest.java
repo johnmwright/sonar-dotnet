2bcdce08f381666b9001df8e0ab5a3ebd2b9666f
John M. Wright <john@wrightfully.com>
2013-09-27 23:16:31 -0500

    extracted resharper plugin as a standalone (not bundled with dotnet core)

---
+++ sonar-dotnet-resharper-plugin/src/test/java/com/wrightfully/sonar/plugins/dotnet/resharper/ReSharperResultParserTest.java
@@ -0,0 +1,114 @@
+/*
+ * Sonar .NET Plugin :: ReSharper
+ * Copyright (C) 2013 John M. Wright
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.wrightfully.sonar.plugins.dotnet.resharper;
+
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.sonar.api.batch.SensorContext;
+import org.sonar.api.resources.Project;
+import org.sonar.api.resources.ProjectFileSystem;
+import org.sonar.api.resources.Resource;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleFinder;
+import org.sonar.api.rules.RuleQuery;
+import org.sonar.api.rules.Violation;
+import org.sonar.plugins.dotnet.api.DotNetResourceBridge;
+import org.sonar.plugins.dotnet.api.DotNetResourceBridges;
+import org.sonar.plugins.dotnet.api.microsoft.MicrosoftWindowsEnvironment;
+import org.sonar.plugins.dotnet.api.microsoft.VisualStudioProject;
+import org.sonar.plugins.dotnet.api.microsoft.VisualStudioSolution;
+import org.sonar.plugins.dotnet.api.utils.ResourceHelper;
+import org.sonar.test.TestUtils;
+
+import java.io.File;
+import java.nio.charset.Charset;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.*;
+
+public class ReSharperResultParserTest {
+
+    private SensorContext context;
+    private DotNetResourceBridge resourcesBridge;
+    private ReSharperResultParser parser;
+    private File resultFile;
+
+    @Before
+    public void init() {
+        context = mock(SensorContext.class);
+        resourcesBridge = mock(DotNetResourceBridge.class);
+        when(resourcesBridge.getLanguageKey()).thenReturn("cs");
+        DotNetResourceBridges bridges = new DotNetResourceBridges(new DotNetResourceBridge[] {resourcesBridge});
+
+        Project project = mock(Project.class);
+        ProjectFileSystem fileSystem = mock(ProjectFileSystem.class);
+        when(fileSystem.getSourceDirs()).thenReturn(Lists.newArrayList(new File("C:\\Sonar\\Example")));
+        when(fileSystem.getSourceCharset()).thenReturn(Charset.forName("UTF-8"));
+        when(project.getFileSystem()).thenReturn(fileSystem);
+        when(project.getLanguageKey()).thenReturn("cs");
+
+        MicrosoftWindowsEnvironment env = mock(MicrosoftWindowsEnvironment.class);
+        VisualStudioProject vsProject = mock(VisualStudioProject.class);
+        VisualStudioSolution solution = mock(VisualStudioSolution.class);
+
+        when(env.getCurrentSolution()).thenReturn(solution);
+        when(vsProject.getName()).thenReturn("Example.Application");
+        when(vsProject.contains(any(File.class))).thenReturn(true);
+        when(solution.getProjectFromSonarProject(any(Project.class))).thenReturn(vsProject);
+        when(solution.getProject(any(File.class))).thenReturn(vsProject);
+
+        when(project.getName()).thenReturn("Example.Application");
+
+        ResourceHelper resourceHelper = mock(ResourceHelper.class);
+        when(resourceHelper.isResourceInProject(any(Resource.class), any(Project.class))).thenReturn(true);
+
+        parser = new ReSharperResultParser(env, project, context, newRuleFinder(), bridges, resourceHelper);
+    }
+
+    @Test
+    public void testParseFileVerifyCallPattern() throws Exception {
+        resultFile = TestUtils.getResource("/solution/Example/resharper-results-example_sln.xml");
+        parser.parse(resultFile);
+
+        // Verify calls on context to save violations
+        verify(context, times(13)).saveViolation(any(Violation.class));
+    }
+
+    private RuleFinder newRuleFinder() {
+        RuleFinder ruleFinder = mock(RuleFinder.class);
+        when(ruleFinder.find((RuleQuery) anyObject())).thenAnswer(new Answer<Rule>() {
+
+            public Rule answer(InvocationOnMock iom) throws Throwable {
+                RuleQuery query = (RuleQuery) iom.getArguments()[0];
+
+                Rule fakeRule = Rule.create(ReSharperConstants.REPOSITORY_KEY, query.getKey(), "Fake rule")
+                        .setConfigKey(query.getConfigKey());
+                return fakeRule;
+
+            }
+        });
+        return ruleFinder;
+    }
+
+}
